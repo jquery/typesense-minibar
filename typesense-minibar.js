4342d7699e32b558cdfd899510fea24c1be05428
Timo Tijhof <krinkle@fastmail.com>
2023-08-25 01:48:52 +0100

    Release 1.1.0

--- typesense-minibar.js
+++ typesense-minibar.js
@@ -1,4 +1,4 @@
-/*! https://github.com/jquery/typesense-minibar 1.0.2 */
+/*! https://github.com/jquery/typesense-minibar 1.1.0 */
 globalThis.tsminibar = function tsminibar (form) {
   const { origin, collection } = form.dataset;
   const group = !!form.dataset.group;
